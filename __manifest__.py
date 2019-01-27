04ec2d745934cb6e3829deb56b207c3fbdb4d443
Paszo <paszo2008@gmail.com>
2019-01-27 15:45:40 +0100

    [ADD] Added an icon and version number

--- __manifest__.py
+++ __manifest__.py
@@ -10,13 +10,13 @@
     """,
 
     'author': "Daniel Reis",
-    'website': "http://www.yourcompany.com",
+    #'website': "http://www.yourcompany.com",
 
     # Categories can be used to filter modules in modules listing
     # Check https://github.com/odoo/odoo/blob/12.0/odoo/addons/base/data/ir_module_category_data.xml
     # for the full list
     'category': 'Uncategorized',
-    'version': '0.1',
+    'version': '12.0.0.1',
 
     # any module necessary for this one to work correctly
     'depends': ['base'],

b033c2cf322ab266894212e6b9f319c50f25ce17
Paszo <paszo2008@gmail.com>
2019-02-01 20:53:46 +0100

    [ADD] Added the button to check ISBN number

--- models/models.py
+++ models/models.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 
 from odoo import models, fields, api
+from odoo.exceptions import Warning
 
 class Book(models.Model):
     _name = 'library.book'
@@ -12,6 +13,30 @@ class Book(models.Model):
     image = fields.Binary('Cover')
     publisher_id = fields.Many2one('res.partner', string='Publisher')
     author_ids = fields.Many2many('res.partner', string='Authors')
+
+    @api.multi
+    def _check_isbn(self):
+        self.ensure_one()
+        digits = [int(x) for x in self.isbn if x.isdigit()]
+        if len(digits) == 13:
+            ponderators = [1, 3] * 6
+            total = sum(a * b for a, b in zip(digits[:12], ponderators))
+            remain = total % 10
+            check = 10 - remain if remain != 0 else 0
+            return digits[-1] == check
+
+    @api.multi
+    def button_check_isbn(self):
+        for book in self:
+            if not book.isbn:
+                raise Warning(
+                'Please provide an ISBN13 for %s' % book.name)
+            if book.isbn and not book._check_isbn():
+                raise Warning(
+                '%s is an invalid ISBN' % book.isbn)
+            return True
+
+
 # class library_app(models.Model):
 #     _name = 'library_app.library_app'
 

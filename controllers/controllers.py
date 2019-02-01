cff9ee2da7ec63938e45957a8caf5483551e111c
Paszo <paszo2008@gmail.com>
2019-02-01 21:06:15 +0100

    [ADD] Added http site

--- controllers/controllers.py
+++ controllers/controllers.py
@@ -1,6 +1,13 @@
 # -*- coding: utf-8 -*-
 from odoo import http
 
+class Books(http.Controller):
+    @http.route('/library/books', auth='user')
+    def list(self, **kwargs):
+        Book = http.requerst.env['library.book']
+        books = Book.search([])
+        return http/request.render(
+            'library_app.book_list_template', {'books': books})
 # class LibraryApp(http.Controller):
 #     @http.route('/library_app/library_app/', auth='public')
 #     def index(self, **kw):

0cbdfaa59d52c0d82c2ffec66a5aa5d94353773c
Paszo <paszo2008@gmail.com>
2019-02-08 20:50:16 +0100

    [ADD] Added changes to library.book model

--- models/models.py
+++ models/models.py
@@ -6,8 +6,40 @@ from odoo.exceptions import Warning
 class Book(models.Model):
     _name = 'library.book'
     _description = 'Book'
-    name = fields.Char('Title', required=True)
+    _order = 'name, date_published desc'
+    #String fields
+    name = fields.Char(
+        'Title',
+        default=None,
+        index=True,
+        help='Book cover title.',
+        readonly=False,
+        required=True,
+        translate=False,
+        )
     isbn = fields.Char('ISBN')
+    book_type = fields.Selection(
+        [('paper','Paperback'),
+        ('hard','Hardcover'),
+        ('electronic','Electronic'),
+        ('other','Other')],
+        'Type')
+    notes = fields.Text('Internal Notes')
+    descr = fields.Html('Description')
+
+    # Numeric fields
+    copies = fields.Integer(default=1)
+    avg_rating = fields.Float('Average Rating', (3, 2))
+    price = fields.Monetary('Price', 'currency_id')
+    currency_id  = fields.Many2one('res.currency') #price helper
+
+    # Date and time fields:
+    date_published = fields.Date()
+    last_borrow_date = fields.Datetime(
+        'Last Borrowed On',
+        default=lambda self: fields.Datetime.now())
+
+    # Other fields
     active = fields.Boolean('Active?', default=True)
     date_published = fields.Date()
     image = fields.Binary('Cover')
